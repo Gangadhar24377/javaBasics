f7a449a2ac3926f807932764abcbd90a55e2b457
Gangadhar24377 <gangadharkambhamettu@gmail.com>
2023-11-23 02:47:50 +0530

    Updated commit

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,21 +4,43 @@ public class Hello {
         System.out.println("Hello World");
 
         boolean isAlien = true;
-        if (isAlien == false) {                                 // code block
+        if (isAlien == true) {                                 // code block
             System.out.println("It is not an alien!");
             System.out.println("Scared of aliens");
         }
 
         int topScore = 80;
-        if (topScore >= 100) {
+        if (topScore < 100) {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 81;
+        int secondTopScore = 95;
         if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than 2nd top scorer and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of conditions are true");
+        }
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is supposed to happen");
 
         }
-    }
 
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
+    }
 }
