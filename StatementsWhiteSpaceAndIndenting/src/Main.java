1f6a9c6709d0163aa8abfdf00d43cd59de03d2f9
Gangadhar24377 <gangadharkambhamettu@gmail.com>
2023-11-24 11:43:26 +0530

    Updating details.txt

--- StatementsWhiteSpaceAndIndenting/src/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+
+        int anotherVar=50;myVariable--;System.out.println("myVariable"+myVariable);
     }
 }
